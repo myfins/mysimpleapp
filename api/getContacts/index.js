16bd804fc49d374943cf33d3945651be68ca176b
APTTUS\shshinde <shshinde@conga.com>
2025-11-07 21:53:07 +0530

    8

--- api/getContacts/index.js
+++ api/getContacts/index.js
@@ -1,6 +1,3 @@
 module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    body: { ok: true, message: "Function runtime works!" },
-  };
+  context.res = { status: 200, body: { ok: true } };
 };
