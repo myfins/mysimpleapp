428b3c78c2c9fa22cbcfa8f27ce0e9e7e9c7e760
APTTUS\shshinde <shshinde@conga.com>
2025-11-07 21:19:16 +0530

    5

--- api/getContacts/index.js
+++ api/getContacts/index.js
@@ -1,8 +1,10 @@
 const fetch = require("node-fetch");
 
 module.exports = async function (context, req) {
-  const SUPABASE_URL = process.env.SUPABASE_URL;
-  const SUPABASE_KEY = process.env.SUPABASE_KEY;
+  //const SUPABASE_URL = process.env.SUPABASE_URL;
+  //const SUPABASE_KEY = process.env.SUPABASE_KEY;
+  const SUPABASE_URL = "https://ckuxsbbqlzqrzkxniajr.supabase.co";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrdXhzYmJxbHpxcnpreG5pYWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MjI1NzYsImV4cCI6MjA3Nzk5ODU3Nn0.Caq7LLpBNX8KXiVmEJZwlrel8os9DjRS_96msLevlho";
   const url = `${SUPABASE_URL}/rest/v1/contact?select=*`;
 
   try {
