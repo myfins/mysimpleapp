2273aa256ac9d4930624db5f2891d887be34b01b
APTTUS\shshinde <shshinde@conga.com>
2025-11-07 22:13:01 +0530

    13

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 async function loadContacts() {
-  const response = await fetch("/api/getContacts");
-  const contacts = await response.json();
-  
-  const tbody = document.querySelector("#contactsTable tbody");
-  tbody.innerHTML = contacts.map(c => `
-    <tr>
-      <td>${c.name}</td>
-      <td>${c.email}</td>
-      <td>${c.phone}</td>
-    </tr>
-  `).join("");
+  try {
+    const res = await fetch('/api/getContacts');
+    const contacts = await res.json();
+
+    const tbody = document.querySelector('#contactsTable tbody');
+    tbody.innerHTML = '';
+
+    contacts.forEach(c => {
+      const row = `
+        <tr>
+          <td>${c.Name}</td>
+          <td>${c.email}</td>
+          <td>${c.phone}</td>
+        </tr>`;
+      tbody.innerHTML += row;
+    });
+  } catch (err) {
+    console.error('Failed to load contacts:', err);
+  }
 }
 
 loadContacts();
